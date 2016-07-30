83ee86e12027fe9a26eeb3ba2db7eed17d6a27e3
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-07-30 15:06:08 -0300

    better powerline fonts install

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,2 +1,4 @@
-cd /tmp/ && git clone git@github.com:powerline/fonts.git
-cd /tmp/fonts/ && ./install.sh
+if [ ! -d "/tmp/powerline-fonts" ]; then
+  cd /tmp/ && git clone git@github.com:powerline/fonts.git powerline-fonts
+  cd /tmp/powerline-fonts/ && ./install.sh
+fi
