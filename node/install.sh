e89bc9cf4e02dfeb3bd6253e11ac56e00ad3a73b
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-08-03 09:59:50 -0300

    including npm-check on node installation

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,5 @@
 if test ! $(which spoof)
 then
-  npm install spoof babel gulp npmrc rnpm webpack-dev-server bower npm react-native-cli webpack -g
+  npm install spoof babel gulp npmrc rnpm webpack-dev-server bower npm react-native-cli webpack npm-check -g
 fi
 
