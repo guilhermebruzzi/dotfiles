22a9cb2cbc4bc23f9700d959c4ee5785f92cde14
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-08-05 10:30:02 -0300

    including nvm in install

--- node/install.sh
+++ node/install.sh
@@ -3,3 +3,7 @@ then
   npm install spoof babel gulp npmrc rnpm webpack-dev-server bower npm react-native-cli webpack npm-check -g
 fi
 
+if test ! $(which nvm)
+then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+fi
