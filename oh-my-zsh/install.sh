d8567d5ebb262f42fd7dee05c79ccc04ce9eb419
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-08-05 10:34:50 -0300

    fix oh-my-zsh install

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,1 +1,2 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -sf "$HOME/.dotfiles/zsh/zshrc.symlink" "$HOME/.zshrc" # relink to our zshrc
