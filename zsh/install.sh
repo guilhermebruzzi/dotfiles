5f1585b84813adb4b08225b70bb8010d92f7c11b
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-08-14 13:16:46 -0300

    install plugins witch are other repositories

---
+++ zsh/install.sh
@@ -0,0 +1,3 @@
+# install plugins
+git clone https://github.com/zsh-users/zsh-completions.git ./custom/plugins/zsh-completions
+git clone https://github.com/tek/zsh-auto-virtualenv.git ./custom/plugins/zsh-auto-virtualenv
