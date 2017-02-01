604e4b54d845cc6aa2c8f89e188140cab1cee15e
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-02-01 14:48:39 -0200

    Add zsh-autosuggestions zsh-syntax-highlighting to plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,3 +6,11 @@ fi
 if [ ! -d ./zsh/custom/plugins/zsh-auto-virtualenv ]; then
   git clone https://github.com/tek/zsh-auto-virtualenv.git ./zsh/custom/plugins/zsh-auto-virtualenv
 fi
+
+if [ ! -d ./zsh/custom/plugins/zsh-autosuggestions ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ./zsh/custom/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d ./zsh/custom/plugins/zsh-syntax-highlighting ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/custom/plugins/zsh-syntax-highlighting
+fi
