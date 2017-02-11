ba68f8fb676d9dd693d357dfbe519e98a10af737
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-02-11 12:28:04 -0200

    Better plugins management

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,16 +1,14 @@
 # install plugins
-if [ ! -d ./zsh/custom/plugins/zsh-completions ]; then
-  git clone https://github.com/zsh-users/zsh-completions.git ./zsh/custom/plugins/zsh-completions
-fi
 
-if [ ! -d ./zsh/custom/plugins/zsh-auto-virtualenv ]; then
-  git clone https://github.com/tek/zsh-auto-virtualenv.git ./zsh/custom/plugins/zsh-auto-virtualenv
-fi
+rm -rf ./zsh/custom/plugins/zsh-auto-completions
+git clone https://github.com/zsh-users/zsh-completions.git ./zsh/custom/plugins/zsh-completions
 
-if [ ! -d ./zsh/custom/plugins/zsh-autosuggestions ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git ./zsh/custom/plugins/zsh-autosuggestions
-fi
 
-if [ ! -d ./zsh/custom/plugins/zsh-syntax-highlighting ]; then
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/custom/plugins/zsh-syntax-highlighting
-fi
+rm -rf ./zsh/custom/plugins/zsh-auto-virtualenv
+git clone https://github.com/tek/zsh-auto-virtualenv.git ./zsh/custom/plugins/zsh-auto-virtualenv
+
+rm -rf ./zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ./zsh/custom/plugins/zsh-autosuggestions
+
+rm -rf ./zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/custom/plugins/zsh-syntax-highlighting
