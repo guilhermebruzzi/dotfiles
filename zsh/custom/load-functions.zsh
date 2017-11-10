702ebe343ed2de5e788c4b44ab734925c415573a
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-11-10 18:26:55 -0200

    Add kubernetes pod as a function

--- zsh/custom/load-functions.zsh
+++ zsh/custom/load-functions.zsh
@@ -1,5 +1,5 @@
 # Load our zsh functions
 
 fpath=( $HOME/.dotfiles/zsh/custom/functions "${fpath[@]}" )
-autoload -Uz c extract gf add-zsh-hook
+autoload -Uz c extract gf add-zsh-hook pod
 autoload -U compinit && compinit
