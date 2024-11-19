e5725da62988854a148459bfce5dc836d268f063
Guilherme Bruzzi <guibruzzi@gmail.com>
2024-11-19 16:54:38 -0300

    Fix git cleanup by making it a function named cleanup_branches

--- zsh/custom/load-functions.zsh
+++ zsh/custom/load-functions.zsh
@@ -1,5 +1,5 @@
 # Load our zsh functions
 
 fpath=( $HOME/.dotfiles/zsh/custom/functions "${fpath[@]}" )
-autoload -Uz c extract gf add-zsh-hook pod rn-menu
+autoload -Uz c extract gf add-zsh-hook pod rn-menu cleanup_branches
 autoload -U compinit && compinit
