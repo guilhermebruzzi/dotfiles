4449e59553b5f13e48374111057faacaf7d4ec62
Guilherme Bruzzi <guibruzzi@gmail.com>
2025-12-02 18:54:52 -0300

    Add create_staging function

--- zsh/custom/load-functions.zsh
+++ zsh/custom/load-functions.zsh
@@ -1,5 +1,5 @@
 # Load our zsh functions
 
 fpath=( $HOME/.dotfiles/zsh/custom/functions "${fpath[@]}" )
-autoload -Uz c extract gf add-zsh-hook pod rn-menu cleanup_branches
+autoload -Uz c extract gf add-zsh-hook pod rn-menu cleanup_branches create_staging
 autoload -U compinit && compinit
