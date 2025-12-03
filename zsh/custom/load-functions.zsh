be257badef0a8ace8c7f9f264dee2dca74071a3b
Guilherme Bruzzi <guibruzzi@gmail.com>
2025-12-03 17:45:01 -0300

    Add delete old tags function

--- zsh/custom/load-functions.zsh
+++ zsh/custom/load-functions.zsh
@@ -1,5 +1,5 @@
 # Load our zsh functions
 
 fpath=( $HOME/.dotfiles/zsh/custom/functions "${fpath[@]}" )
-autoload -Uz c extract gf add-zsh-hook pod rn-menu cleanup_branches create_staging
+autoload -Uz c extract gf add-zsh-hook pod rn-menu cleanup_branches create_staging delete_remote_old_tags
 autoload -U compinit && compinit
